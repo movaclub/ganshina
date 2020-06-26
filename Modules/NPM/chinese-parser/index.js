fb44099d6d9bb4a3fb1b959d7aba2c30bcee74e2
FedirM <tedmoster@gmail.com>
2020-06-26 16:28:23 +0300

    Test

---
+++ Modules/NPM/chinese-parser/index.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} data - Chinese text
+ * @param {number} maxWordLength - The max amount characters in word
+ * @param {boolean} sortToLength - Set if needs to sort the groups by length
+ * @return {Array} 
+ */
+exports.parse = function(data, maxWordLength = 8, sortToLength = false){
+    if( data instanceof String ) return new Error('String object expects as a first parameter.');
+    if( maxWordLength < 1 || maxWordLength > 999 ) return new Error('Max word length must be in range [1;999]');
+    if( !data ) return new Array(0);
+
+    const splitRegxp = new RegExp(/[\uFF0C\uFE10\uFE50\u1F101-\u1F10A\u0000-\u007F\u2000-\u206F\u2E00-\u2E7F\u3000-\u303F\u3200-\u32FF\u3300-\u33FF\uFE30-\uFE4F\u12400-\u1247F\u16FE0-\u16FFF \w\s]+/);
+    const sentences = data.split(splitRegxp).filter((elem) => { return !!elem; });
+
+
+    let resArray = new Array(0);
+    let tmpObj = {};
+
+    for(let sentence of sentences){
+        tmpObj.sentence = sentence;
+        tmpObj.words = [];
+        if( sortToLength ) tmpObj.groups = {};
+        let start = 0;
+
+        for(;start < sentence.length; start++){
+            let amount = ( (start + maxWordLength) < sentence.length)?start + maxWordLength:sentence.length - start;
+            for(;amount > 0; amount--){
+                if( sortToLength ) {
+                    if( amount.toString() in tmpObj.groups ) {
+                        tmpObj.groups[ amount.toString() ].push(sentence.substr(start, amount));
+                    } else {
+                        tmpObj.groups[ amount.toString() ] = new Array(0);
+                    }
+                }
+                tmpObj.words.push( sentence.substr(start, amount) );
+            }
+        }
+        resArray.push( tmpObj );
+    }
+    return resArray;
+}
